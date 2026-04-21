e19f2857074ce0676d144eaa06b75124f6aa86aa
Anshu Poudel <anshupoudel@MacBook-Pro-202.local>
2026-04-21 15:57:33 -0700

    added dataset

--- Data Cleaning.r
+++ Data Cleaning.r
@@ -2,4 +2,8 @@ rm(list = ls())
 library(tidyverse)
 library(dplyr)
 
+#Open working directory 
 data_dir <- "/Users/anshupoudel/EPI-514-Project"
+
+#Download dataset
+#brfss_21_clean <- read.csv(file.path(data_dir, "brfss_21_clean.csv"))
