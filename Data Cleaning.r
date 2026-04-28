0ade8ddef24d781e6ad6b50e17d919d25c784cd1
Tharu Lansakaranayake <ithiril@uw.edu>
2026-04-27 22:29:45 -0700

    loaded libraries and added comments for filter

--- Data Cleaning.r
+++ Data Cleaning.r
@@ -2,11 +2,8 @@ rm(list = ls())
 library(tidyverse)
 library(dplyr)
 
-<<<<<<< HEAD
 #Open working directory 
 data_dir <- "/Users/anshupoudel/EPI-514-Project"
 
 #Download dataset
 #brfss_21_clean <- read.csv(file.path(data_dir, "brfss_21_clean.csv"))
-=======
->>>>>>> 0ba8051 (first push)
