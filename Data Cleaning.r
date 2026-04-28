ff0f31cbe958137bd1bab11eb5a36f8a9e00a8f9
Anshu Poudel <anshupoudel@MacBook-Pro-202.local>
2026-04-27 22:23:34 -0700

    subsetting for states and creating table of outcomes for power calcuation

--- Data Cleaning.r
+++ Data Cleaning.r
@@ -1,12 +1,27 @@
+install.packages("here")
 rm(list = ls())
-library(tidyverse)
+
+# Load libraries ----------------------------------------------------------
 library(dplyr)
+library(tidyverse)
+library(haven)
+library(survey)
+library(ggplot2)
+library(epiR)
+library(table1)
+library(here) 
 
-<<<<<<< HEAD
-#Open working directory 
-data_dir <- "/Users/anshupoudel/EPI-514-Project"
+# Open dataset ------------------------------------------------------------
+brfss_24 <- read_xpt(here("LLCP2024.XPT "))
 
-#Download dataset
-#brfss_21_clean <- read.csv(file.path(data_dir, "brfss_21_clean.csv"))
-=======
->>>>>>> 0ba8051 (first push)
+# Filtering for states ------------------------------------------------------------
+states_to_exclude <- c(8, 9, 12, 17, 21, 23, 26, 29, 31, 32, 36, 39, 40, 41, 42, 48, 51, 53, 56, 66) 
+
+study_data <- brfss_24 %>% 
+  filter(!(`_STATE` %in% states_to_exclude))
+
+table(study_data$`_STATE`)
+
+# Tooth loss variable count for power calculation ------------------------------------------------------------
+table(study_data$`RMVTETH4`, useNA = "ifany")
+                          
